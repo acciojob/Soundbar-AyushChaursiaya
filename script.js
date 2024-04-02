8bd152cd67bb17b05817b3089aef142eedba2d08
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-04-02 13:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+// Wait for the DOM content to be fully loaded before executing the script
 document.addEventListener('DOMContentLoaded', function() {
+  // Select all buttons with the class 'btn'
   const buttons = document.querySelectorAll('.btn');
+  // Variable to store the audio element
   let audio = null;
 
+  // Add event listeners to each button
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function() {
+      // Get the value of the 'data-sound' attribute of the clicked button
       const soundFile = this.getAttribute('data-sound');
+      // Check if the button has a 'data-sound' attribute
       if (soundFile) {
+        // If the button has a 'data-sound' attribute, play the corresponding sound
         playSound(soundFile);
       } else {
+        // If the button does not have a 'data-sound' attribute, stop the audio
         stopSound();
       }
     });
   }
 
+  // Function to play a sound
   function playSound(soundFile) {
+    // Stop any currently playing audio
     stopSound();
-    audio = new Audio(`sounds/${soundFile}`);
-    audio.play();
-  }
-
-  function stopSound() {
-    if (audio)
+    // Create a new audio element with the specified sound file
+    audio = new Audio(`sounds/${sound
